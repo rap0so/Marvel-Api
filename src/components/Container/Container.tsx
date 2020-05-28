a4d13a5bbd9e15a3d3128200fda25ae6a0c48e88
William G <william.godoy53@gmail.com>
2020-05-28 12:39:23 -0300

    test(container): garantee style

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -4,7 +4,13 @@ import { BoxProps } from 'reflexbox';
 import ContainerStyled from './ContainerStyled';
 
 const Container: FC<BoxProps> = (props) => (
-  <ContainerStyled m="auto" maxWidth="1200px" p={[2, 3]} {...props}>
+  <ContainerStyled
+    m="auto"
+    maxWidth="1200px"
+    p={[2, 3]}
+    {...props}
+    data-testid="container"
+  >
     {props.children}
   </ContainerStyled>
 );
