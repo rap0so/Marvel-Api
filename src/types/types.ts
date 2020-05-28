22894844bca6427bc4ccd291ee23f886eb61006f
William G <william.godoy53@gmail.com>
2020-05-28 01:25:59 -0300

    chore(types): add some general types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,10 +1,13 @@
-export type TMarvelPublicTypes =
-  | 'characters'
-  | 'comics'
-  | 'creators'
-  | 'events'
-  | 'series'
-  | 'stories';
+export type TMarvelPublicTypes = 'characters';
+
+// TODO: Improve project to receive all kind of data
+// export type TMarvelPublicTypes =
+//   | 'characters'
+//   | 'comics'
+//   | 'creators'
+//   | 'events'
+//   | 'series'
+//   | 'stories';
 
 export type TGeneralCallback = (arg?: any) => any;
 
@@ -12,4 +15,19 @@ export type TGeneralCallThatReturnsAnObject = (
   arg?: any,
 ) => {
   [key: string]: any;
+};
+
+export type TMarvelPublicResult = {
+  id: number;
+  comics: any;
+  description: string;
+  events: any;
+  modified: string;
+  name?: string;
+  resourceURI: string;
+  series: any;
+  stories: any;
+  title?: string;
+  thumbnail: { path: string; extension: string };
+  urls: { type: string; url: string }[];
 };
