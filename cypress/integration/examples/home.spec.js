de626fd67edaf38318bf3028efae434019da653b
William G <william.godoy53@gmail.com>
2020-05-28 18:05:21 -0300

    test(home): verify base user usage

---
+++ cypress/integration/examples/home.spec.js
@@ -0,0 +1,24 @@
+describe('Home', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('should go to `/` when `/` is visited', () => {
+    cy.location('pathname').should('eq', '/');
+  });
+
+  it('should search when type on search input', () => {
+    cy.get('input').type('Hulk');
+
+    cy.get('[data-testid="card"] h2').should((elem) => {
+      expect(elem.first()).to.have.text('Hulk');
+    });
+  });
+
+  it.only('should redirect when click on card', () => {
+    cy.location('pathname').should('eq', '/');
+    cy.get('[data-testid="card"]').eq(4).click();
+
+    cy.url().should('include', '/series/');
+  });
+});
