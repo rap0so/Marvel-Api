893ec68994b561b9641f66a38023004b8aae8228
William G <william.godoy53@gmail.com>
2020-05-28 03:51:53 -0300

    test(title): base test

---
+++ src/components/Title/title.test.tsx
@@ -0,0 +1,22 @@
+import { cleanup, screen, render } from '@testing-library/react';
+import React from 'react';
+
+import Title from '.';
+
+afterEach(cleanup);
+
+it('should render cmp', () => {
+  const { getByText } = render(<Title>Title test</Title>);
+
+  expect(getByText('Title test')).toBeInTheDocument();
+});
+
+it('should receive title props', () => {
+  const { getByText } = render(<Title fontSize="16px">Big Title</Title>);
+
+  const titleElement = getByText('Big Title');
+  const stylesFromTitle = window.getComputedStyle(titleElement);
+
+  expect(stylesFromTitle.getPropertyValue('font-size')).toBe('16px');
+  expect(stylesFromTitle.getPropertyValue('font-weight')).toBe('bold');
+});
